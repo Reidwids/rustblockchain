9a215fa9ff40a6340a2e12312efadf61023f7fd3
reidwids <reidwids@gmail.com>
2025-05-02 23:20:51 -0400

    implement create wallet in wasm

--- dcoin_wasm/src/lib.rs
+++ dcoin_wasm/src/lib.rs
@@ -1,6 +1,22 @@
-use wasm_bindgen::prelude::*;
+use core_lib::wallet::Wallet;
+use serde::{Deserialize, Serialize};
+use serde_wasm_bindgen::to_value;
+use wasm_bindgen::{JsValue, prelude::wasm_bindgen};
+
+#[derive(Serialize, Deserialize)]
+pub struct JsWallet {
+    pub private_key: String,
+    pub public_key: String,
+}
 
 #[wasm_bindgen]
-pub fn test(str: &str) -> String {
-    format!("String + test = {}test", str)
+pub fn create_wallet() -> JsValue {
+    let wallet = Wallet::new();
+
+    let js_wallet = JsWallet {
+        private_key: wallet.private_key().display_secret().to_string(),
+        public_key: wallet.pub_key().to_string(),
+    };
+
+    to_value(&js_wallet).unwrap()
 }
